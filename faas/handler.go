e9ed31396ddd2cdf2c34162f0cf422e0035e452a
Luca Casonato <lucacasonato@yahoo.com>
2019-07-09 02:41:51 +0200

    another deploy

--- faas/handler.go
+++ faas/handler.go
@@ -18,5 +18,5 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hello %s!", string(input))))
+	w.Write([]byte(fmt.Sprintf("Hello %s!!!", string(input))))
 }
